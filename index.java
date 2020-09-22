aa1acf9608461d34676dd842905feb4ad844a77e
Rhonda04 <rpoole10@icloud.com>
2020-09-22 21:14:57 +0000

    My bug Buggy

--- index.java
+++ index.java
@@ -11,14 +11,26 @@ void draw() {
  
   //Make a creature!!! 
   //all of your programming should be after this 👍
-  
- 
-  
-  
- 
+//Buggys body
+ellipse(200,200,100,200)
+fill(255, 165, 0)
 
-  
- 
-	
+//Buggys Head
+ellipse(200,80,50,50)
+
+//Buggys eyes
+ellipse(190,70,10,10)
+ellipse(210,70,10,10)
+
+// Buggys arms
+line(155,150,30,90)
+line(160,180,100,100)
+line(250,200,295,80)
+line(250,175,295,30)
+
+//Buggys mouth
+rect(200,95,15,10)
+
+
 
 }
